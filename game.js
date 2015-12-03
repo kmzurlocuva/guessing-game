90609087732e36682c9a14176e671a6a35993920
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2015-12-03 11:03:00 -0800

    added for and while loops

--- game.js
+++ game.js
@@ -4,8 +4,8 @@ alert('Nice to meet you ' + user + '!');
 var counter = 0;
 var state = prompt('Are you from WI?');
 
-
-if (state === 'yes' || 'YES') {
+  state = state.toLowerCase();
+if (state === 'yes' || state === 'y') {
 alert(state + ", I am a cheesehead from Wisconsin.");
 counter = counter + 1;
 } else {
@@ -13,7 +13,8 @@ alert("I am from Wisconsin")
 }
 
 var kid = prompt('Do you have a kid?');
-if (kid === 'yes' || 'YES') {
+  kid = kid.toLowerCase();
+if (kid === 'yes' || kid === 'y') {
 alert(kid + ", I have a kid named July");
 counter = counter + 1;
 } else {
@@ -22,7 +23,9 @@ alert("I have a kid");
 console.log(kid);
 
 var decade = prompt('Do you love being outside?');
-if (decade === 'yes' || 'YES') {
+  decade = decade.toLowerCase();
+  console.log("This should be all lower case...: " + decade);
+if (decade === 'yes' || decade === 'y') {
 alert(decade + ", I live for nature");
 counter = counter + 1;
 } else {
@@ -37,12 +40,47 @@ alert("You kinda got it");
 } else {
   alert("You don't know me very well at all.");
 }
-var age = prompt('How old are you?');
+var age = parseInt(prompt('How old are you?'));
+console.log("age is this type of data, after parseInt:" + typeof(age));
 if (age === 36) {
+  console.log("I'm in the if statement now.");
   alert("You're right!");
 } else if (age > 36) {
   alert("Harsh.  Burn.");
 } else {
-  alert("Nope, I'm 36.")
+  console.log("I'm in the else statement.");
+  alert("Nope, I'm 36.");
+}
+
+var answer = 36;
+var user = parseInt(prompt('What is your number?'));
+  while (user !== answer) {
+    if (user > answer) {
+      alert("Too high.");
+      user = parseInt(prompt('What is your number?'));
+    } else {
+    alert("You are wrong.");
+    user = parseInt(prompt('What is your number?'));
+    }
+}
+alert("You got it!");
+
+
+function arrayGame() {
+  var homeTown = prompt('What cities have I visited this year?');
+  var cities = ['madison','green bay','la crosse', 'milwaukee'];
+  homeTown = homeTown.toLowerCase();
+  console.log(cities);
+for (var i = 0; i < cities.length; i++) {
+  if (homeTown === cities[i]) {
+    console.log("this should be correct...");
+    alert("Yep, I grew up in Wisconsin.");
+    return;
+  } else {
+    alert("Nope, not this year.");
+    return;
+  }
 
 };
+}
+arrayGame();
